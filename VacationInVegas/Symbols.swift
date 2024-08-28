2e58168ad02bacce83597574a43f2d7b46441f40
Nithish <nithish@Nexts-iMac.local>
2024-08-29 00:16:03 +0530

    main

---
+++ VacationInVegas/Symbols.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  VacationInVegas
+//
+//  Created by Nithish on 28/08/24.
+//
+
+import SwiftUI
+
+struct Symbols: View {
+    @State private var shouldIBounce = false
+    @State private var shouldIRotate = false
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .symbolEffect(.pulse)
+            
+            Image(systemName: "airplane")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .symbolEffect(.pulse)
+            
+            Image(systemName: "wifi")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.purple)
+                .symbolEffect(.variableColor.reversing)
+            
+            Image(systemName: "person.crop.circle.badge.plus")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .symbolRenderingMode(.multicolor)
+                .symbolEffect(.bounce,value: shouldIBounce)
+                .onTapGesture {
+                    shouldIBounce.toggle()
+                }
+            
+            Image(systemName: "cloud.sun.rain.fill")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.gray,.yellow,.mint)
+                .symbolEffect(.bounce,value: shouldIBounce)
+                .onTapGesture {
+                    shouldIBounce.toggle()
+                }
+            
+            Image(systemName: "arrow.clockwise.square")
+                .font(.largeTitle)
+                .imageScale(.large)
+//                .foregroundStyle(.blue.mix(with: .red, by: 0.25))
+                .symbolEffect(.bounce,value: shouldIRotate)
+                .onTapGesture {
+                    shouldIRotate.toggle()
+                }
+            
+            Image(systemName: "sun.max.fill")
+                .font(.largeTitle)
+                .imageScale(.large)
+                .foregroundStyle(.yellow)
+                .symbolEffect(.variableColor.reversing)
+                .onTapGesture {
+                    shouldIRotate.toggle()
+                }
+ 
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    Symbols()
+}
---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by Nithish on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
